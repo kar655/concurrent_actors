e8aa1c07edb794ad2cbdbbd0e66793b3c87a2a61
kar655 <karol123zag@gmail.com>
2021-01-27 13:28:41 +0100

    Add message wait to parent in hello handler

--- silnia.c
+++ silnia.c
@@ -5,6 +5,7 @@
 
 #define MSG_CALCULATE (message_type_t)0x1
 #define MSG_WAIT (message_type_t)0x2
+// todo wiadomosc na czyszczenie zasobow?
 
 // All actors will share same role.
 role_t actor_role;
@@ -26,10 +27,13 @@ typedef struct {
     int last_step; // n -> n
 } factorial_info_t;
 
+
 // Każdy aktor ma otrzymywać w komunikacie dotychczas obliczoną częściową silnię k! wraz z liczbą k,
 // tworzyć nowego aktora i wysyłać do niego (k+1)! oraz k+1.
 // Po końcowego n! wynik powinien zostać wypisany na standardowe wyjście.
 
+// Hello message handler.
+// Saves parent's id and sends MSG_WAIT to parent.
 void message_hello(void **stateptr, size_t nbytes, void *data) {
     actor_id_t parent_id = (actor_id_t) data;
 
@@ -41,23 +45,22 @@ void message_hello(void **stateptr, size_t nbytes, void *data) {
 
     printf("FINISHED message_hello handler with parent: %ld\n", parent_id);
 
-
-// TODO TUTAJ WYSYLANIE WAIT DO OJCA
-
     // Sending message MSG_WAIT to parent to get next state of factorial.
-    message_t message = {.message_type = MSG_WAIT,
-                         .nbytes = sizeof(void *),
-                         .data = (void *) actor_id_self()};
+    message_t message = {
+            .message_type = MSG_WAIT,
+            .nbytes = sizeof(void *),
+            .data = (void *) actor_id_self()};
 
     int error_code = send_message(current_state->parent_id, message);
     assert(error_code == 0);
+
+    printf("SENT MSG_WAIT to parent: %ld\n", parent_id);
 }
 
 // Calculates factorial. If not finished makes new actor.
 void calculate_factorial(void **stateptr, size_t nbystes, void *data) {
     factorial_info_t *current_factorial = (factorial_info_t *) data;
-//    factorial_info_t *my_state = (factorial_info_t *) stateptr;
-//    my_state = current_factorial;
+    *stateptr = current_factorial;
 
     if (current_factorial->last_step == current_factorial->current_factor) {
         // finished, can print
@@ -65,9 +68,10 @@ void calculate_factorial(void **stateptr, size_t nbystes, void *data) {
     }
     else {
         // Creates new actor. Now should wait for it to be ready.
-        message_t message = {.message_type = MSG_SPAWN,
-                             .nbytes = sizeof(&actor_role),
-                             .data = (void *) &actor_role};
+        message_t message = {
+                .message_type = MSG_SPAWN,
+                .nbytes = sizeof(role_t *),
+                .data = (void *) &actor_role};
 
         int error_code = send_message(actor_id_self(), message);
         assert(error_code == 0);
@@ -79,25 +83,29 @@ void calculate_factorial(void **stateptr, size_t nbystes, void *data) {
 // Parent sends next factorial state to actor with id in *data.
 void son_waiting_for_factorial(void **stateptr, size_t nbystes, void *data) {
     factorial_info_t *current_factorial = (factorial_info_t *) stateptr;
+    actor_id_t child_id = (actor_id_t) data;
 
     int next_factor = current_factorial->current_factor + 1;
-    factorial_info_t next = {.current_factorial = current_factorial->current_factorial * next_factor,
+    factorial_info_t next = {
+            .current_factorial = current_factorial->current_factorial * next_factor,
             .current_factor = next_factor,
             .last_step = current_factorial->last_step};
 
-    message_t message = {.message_type = MSG_CALCULATE,
-                         .nbytes = sizeof(factorial_info_t *),
-                         .data = (void *) &next};
+    message_t message = {
+            .message_type = MSG_CALCULATE,
+            .nbytes = sizeof(factorial_info_t *),
+            .data = (void *) &next};
 
-    printf("sending message to parent that I'm ready\n");
+    printf("SENDING MSG_CALCULATE to child %ld\n", child_id);
 
-    int error_code = send_message(current_factorial->parent_id, message);
+    int error_code = send_message(child_id, message);
     assert(error_code == 0);
 }
 
 int main() {
     int n;
-    scanf("%d", &n);
+//    scanf("%d", &n);
+    n = 5;
     printf("brute_factorial(%d) = %d\n", n, brute_factorial(n));
 
     printf("Teraz aktorzy xD!\n");
@@ -105,45 +113,36 @@ int main() {
     int error_code;
     actor_id_t actor_id;
 
-    actor_role = (role_t) {.nprompts = 2,
-            .prompts = (act_t *) message_hello};
+    actor_role.nprompts = 3;
+
+    act_t *handlers = (act_t *) malloc(actor_role.nprompts * sizeof(act_t));
+    handlers = (act_t *) {message_hello, calculate_factorial, son_waiting_for_factorial};
+    actor_role.prompts = handlers;
 
 
     error_code = actor_system_create(&actor_id, &actor_role);
     assert(error_code == 0);
 
-    printf("First actor id = %ld", actor_id);
+    printf("First actor id = %ld\n", actor_id);
 
-    factorial_info_t initial_state = {.current_factorial = 1,
-                                      .current_factor = 0,
-                                      .last_step = n};
+    factorial_info_t initial_state = {
+            .parent_id = -1,
+            .current_factorial = 1,
+            .current_factor = 0,
+            .last_step = n};
 
-    message_t message = {.message_type = MSG_CALCULATE,
-                         .nbytes = sizeof(initial_state),
-                         .data = (void *) &initial_state};
+    message_t message = {
+            .message_type = MSG_CALCULATE,
+            .nbytes = sizeof(factorial_info_t *),
+            .data = (void *) &initial_state};
 
     error_code = send_message(actor_id, message);
     assert(error_code == 0);
 
 
-
-
-
-//    actor_system_join(actor_id);
-
-// ------------------------------------------------------------------------------------------
-
-//    printf("START\n");
-////    int result = actor_system_create(NULL, NULL);
-////    printf("actor_system_create result = %d\n", result);
-//
-//    int error_code;
-//    actor_id_t first_actor_id = -1;
-//    role_t actor_role = (role_t) {.nprompts = 2, .prompts = {calculate, calculate}};
-//
-//    int result = actor_system_create(&first_actor_id, &actor_role);
-//    printf("actor_system_create result = %d\n", result);
-//    printf("new actor id: %ld\n", first_actor_id);
+    actor_system_join(actor_id);
+    printf("THIS SHOULD BE LAST MESSAGE\n");
+//    free(handlers);
 
     return 0;
 }
